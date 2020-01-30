c53dd8b5fbb3910e05abcc1e7499ab45482ca3cc
SNeuroble <stephanie.noble@yale.edu>
2020-01-30 17:31:05 -0500

    Cleaned up

--- config_files/cfg.m
+++ config_files/cfg.m
@@ -1,28 +1,19 @@
 %% Definitions for running NBS benchmarking and summarization
 
-% script directories
-nbs_path='/mridata2/home2/smn33/scripts/NBS_benchmarking/NBS1.2';
-% nbs_addon_path='/mridata2/home2/smn33/scripts/NBS_benchmarking';
-%nbs_path='/Volumes/GoogleDrive/My Drive/Steph-Lab/Misc/Software/scripts/Matlab/fmri/NBS1.2';
-%nbs_addon_path='/Volumes/GoogleDrive/My Drive/Steph-Lab/Misc/Software/scripts/Matlab/myscripts/cNBS';
-% dir for other scripts used (structure_data, draw_atlas_boundaries, summarize_matrix_by_atlas)
-other_scripts_directory='/mridata2/home2/smn33/scripts/matlab/myscripts/general_mri_new/general_mri/';
-%other_scripts_directory='/Volumes/GoogleDrive/My Drive/Steph-Lab/Misc/Software/scripts/Matlab/myscripts/general_mri';
+% script directories (other_scripts are misc stuff used for summarization - structure_data, draw_atlas_boundaries, summarize_matrix_by_atlas)
+nbs_path='/Users/steph/Steph-Lab/Misc/Software/scripts/Matlab/fmri/NBS1.2';
+other_scripts_directory='/Volumes/GoogleDrive/My Drive/Steph-Lab/Misc/Software/scripts/Matlab/myscripts/general_mri';
 
-% input data - assumes square matrix (typically HCP toy data)
-% data_path='/mnt/store1/mridata2/mri_group/smn33_data/hcp/data_01ffd_v7_3.mat';
-data_path='/Volumes/GoogleDrive/My Drive/Steph-Lab/Misc/ConstableLab/MRRC Neuroinformatics/resources/scripts/cpm_hackathon/test_data/HCP900_rest_n50.mat';
+%nbs_path='/mridata2/home2/smn33/scripts/NBS_benchmarking/NBS1.2'; % if on server
+%other_scripts_directory='/mridata2/home2/smn33/scripts/matlab/myscripts/general_mri_new/general_mri/'; % if on server
 
-% output_dir
-% output_dir='/mnt/store1/mridata2/mri_group/smn33_data/hcp/NBS_benchmarking_results/';
-output_dir='/Users/steph/Steph-Lab/NBS_benchmarking/NBS_benchmarking/results_benchmarking/';
+% data paths - input data is assumed to be n_nodes x n_nodes x n_subjects typically HCP toy data)
+data_path='/Users/steph/Steph-Lab/Misc/ConstableLab/MRRC Neuroinformatics/resources/scripts/cpm_hackathon/test_data/HCP900_rest_n50.mat';
+output_dir='/Users/steph/Steph-Lab/NBS_benchmarking/results_benchmarking/';
 
-% input previous results - for summarization only
-%use_previous_results=0; % if this is true, then will skip processing and load previously calculated results 
-previous_results_basename='nbs_benchmark_results__Size_Intensity_01082020_1416.mat';
-% previous_results_basename='nbs_benchmark_results__Size_Extent_12122019_2128.mat';
-% previous_results_basename='nbs_benchmark_results__Size_Intensity_12182019_1906.mat';
-% previous_results_basename='nbs_benchmark_results__TFCE_12132019_1042.mat';
+% data_path='/mnt/smn33_data/hcp/data_01ffd_v7_3.mat'; % if mounted locally
+% data_path='/mnt/store1/mridata2/mri_group/smn33_data/hcp/data_01ffd_v7_3.mat'; % if on server 
+% output_dir='/mnt/store1/mridata2/mri_group/smn33_data/hcp/NBS_benchmarking_results/'; % if on server
 
 % user-defined - resampling parameters (TODO: consider putting in one variable like UI)
 testing=1; % developers only - speeds up analyses for troubleshooting but inappropriate for inference
@@ -34,23 +25,21 @@ n_repetitions=1000;
 n_subs_subset=40; % size of subset is full group size (N=n*2 for two sample t-test or N=n for one-sample)
 
 % user-defined - NBS parameters
+% TODO: right now dmat/contrast only designed for t-test, and edge_groups can only be Shen atlas
 nbs_method='Run NBS'; % TODO: revise to include vanilla FDR
 nbs_contrast='[1,-1]'; % Do not change - right now this is the only option
-% dmat - TBD - right now the above contrast is the only option
 nbs_test_stat='t-test'; % alternatives are one-sample and F-test
 n_perms='1000'; % previously: '5000'
 zthresh_first_level='3.1'; % p=0.01
 pthresh_second_level='0.05';
 cluster_stat_type='Size'; % 'Size' | 'TFCE' | 'Constrained' | 'SEA' - smn
 cluster_size_type='Intensity'; % 'Intensity' | 'Extent' - only relevant if stat type is 'Size'
-% edge_groups - TBD - right now uses default Shen grouping
 nbs_exchange='';
 
 % user-defined: visualization parameters
 do_visualization=1;
 
-% DEVELOPERS ONLY
-% Use a small subset of reps and perms to speed up troubleshooting but inappropriate for inference
+% DEVELOPERS ONLY - Use a small subset of reps and perms to speed up troubleshooting but inappropriate for inference
 if testing 
     n_repetitions=70;   
     n_perms='20';
