8b72ab29ecbb8091c53b3836a98f00c300facfe0
SNeuroble <stephanie.noble@yale.edu>
2020-02-13 16:55:09 -0500

    Added negative contrasts and updated parameters

--- config_files/setparams.m
+++ config_files/setparams.m
@@ -9,16 +9,16 @@ non_task_condition='REST'; % for FPR or TPR contrast
 encoding='RL'; % TODO: combine
 
 % Resampling parameters
-n_workers=4; % num parallel workers for parfor, best to use # workers = # cores
+n_workers=2; % num parallel workers for parfor, best to use # workers = # cores
 mapping_category='subnetwork'; % for cNBS
-n_repetitions=1000;
-n_subs_subset=20; % size of subset is full group size (N=n*2 for two sample t-test or N=n for one-sample)
+n_repetitions=500;
+n_subs_subset=40; % size of subset is full group size (N=n*2 for two sample t-test or N=n for one-sample)
 
 % NBS parameters
 % TODO: right now dmat/contrast only designed for t-test, and edge_groups can only be Shen atlas
 nbs_method='Run NBS'; % TODO: revise to include vanilla FDR
 nbs_test_stat='t-test'; % alternatives are one-sample and F-test - don't change for now, bc dmat and contrast based on this
-n_perms='2000'; % previously: '5000'
+n_perms='1000'; % previously: '5000'
 tthresh_first_level='3.1'; % corresponds with p=0.005-0.001 (DOF=10-1000)
 pthresh_second_level='0.05';
 cluster_stat_type='SEA'; % 'Size' | 'TFCE' | 'Constrained' | 'SEA' - smn
