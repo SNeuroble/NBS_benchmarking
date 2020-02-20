88352369e7174bbf85b99520f141b3e3aea43733
SNeuroble <stephanie.noble@yale.edu>
2020-02-20 09:58:35 -0500

    Reorganized statistics folders

--- NBS_benchmarking_addon/cNBS_functions/cNBS_to_full.m
+++
@@ -1,6 +0,0 @@
-function cNBS_to_full(cNBS_vector, edge_groups)
-% TBD: converts cNBS vector to full matrix (e.g., if cNBS_vector=55x1 vector of
-% positives and edge_groups=268x268 matrix from UI.edge groups
-
-
-end
