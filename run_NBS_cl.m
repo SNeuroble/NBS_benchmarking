d82ad269e9de99f393814c9bd092e9e009a9c1cc
SNeuroble <stephanie.noble@yale.edu>
2020-07-13 20:24:32 -0400

    Added mapping of cNBS results to the edge level.

--- run_NBS_cl.m
+++ run_NBS_cl.m
@@ -29,10 +29,27 @@ UI.edge_groups.ui=edge_groups_file;
 UI.exchange.ui=exchange;
 UI.matrices.ui=data_file;
 
-% run command line NBS (TODO: maybe rename NBSrun_cl)
+% Run command line NBS (TODO: maybe rename NBSrun_cl)
 nbs=NBSrun_smn(UI);
 
-% results for cNBS
+% Significant results
 if ischar(pthresh_second_level); pthresh_second_level=str2num(pthresh_second_level); end
 sig_results=nbs.NBS.pval<pthresh_second_level;
+
+% Map significant cNBS results to the edge level
+% Note: Requires edge_groups_file to be a numerical matrix workspace 
+% variable. cNBS results will be 1 x n subnetworks, where the first entry 
+% corresponds to edges in edge group 1, second to edges in edge group 2, etc
+
+if strcmp(cluster_stat_type,'Constrained')
+    if ischar(edge_groups_file)
+    else
+        sig_edge_results=edge_groups;
+        it=1;
+        for i=unique(nonzeros(edge_groups))'
+            sig_edge_results(edge_groups==i)=sig_results(it);
+            it=it+1;
+        end
+    end
+end
 
