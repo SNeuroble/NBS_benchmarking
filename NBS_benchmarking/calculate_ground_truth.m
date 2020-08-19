c0b80e6064f5cc016ff459e0f6a2fa1544b10fd0
SNeuroble <stephanie.noble@yale.edu>
2020-08-19 16:03:04 -0400

    Now using both tasks in filename

--- NBS_benchmarking/calculate_ground_truth.m
+++ NBS_benchmarking/calculate_ground_truth.m
@@ -21,9 +21,9 @@ if strcmp(UI.statistic_type.ui,'Size'); size_str=['_',UI.size.ui];
 else; size_str='';
 end
 if testing; test_str='_testing'; else test_str=''; end
-if do_TPR; condition_str=rep_params.task; else condition_str=rep_params.non_task; end
+if use_both_tasks; condition_str=[rep_params.task1,'_v_',rep_params.task2]; else condition_str=rep_params.task1; end
 
-output_filename=[output_dir,'nbs_ground_truth__',condition_str,'_',UI.statistic_type.ui,size_str,test_str,'_',datestr(now,'mmddyyyy_HHMM'),'.mat'];
+output_filename=[output_dir,'ground_truth__',condition_str,'_',UI.statistic_type.ui,size_str,test_str,'_',datestr(now,'mmddyyyy_HHMM'),'.mat'];
 fprintf('Saving results in %s\n',output_filename)
 save(output_filename,'edge_stats','cluster_stats','UI_light','rep_params');
 
