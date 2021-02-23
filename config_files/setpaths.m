14d321135c7184223fec747513c60a359a9fa272
SNeuroble <stephanie.noble@yale.edu>
2021-02-23 15:22:08 -0500

    Superficial reorganization

--- config_files/setpaths.m
+++ config_files/setpaths.m
@@ -13,49 +13,50 @@
 data_origin='farnam'; % 'farnam' or 'mrrc' (when mounted on remote)
 
 switch getenv('USER')
-
-case 'smn33'
-     
-    switch data_origin
-    case 'mrrc'
-        data_dir='/data15/mri_group/smn33_data/hcp_1200/matrices/'; % symlink to '/mnt/dustin/data/S1200/matrices/S1200/matrices/'
-        output_dir='/data15/mri_group/smn33_data/NBS_benchmarking_results/';
-	nbs_dir='/mridata2/home2/smn33/scripts/NBS1.2';
-    	other_scripts_dir='/mridata2/home2/smn33/scripts/matlab/myscripts/general_mri_new/general_mri'; 
-
-    case 'farnam'
-        data_dir='/home/smn33/project/HCP_S1200/';
-        output_dir='/home/smn33/project/benchmarking_results/';
-        nbs_dir='/home/smn33/scripts/NBS1.2';
-   	other_scripts_dir='/home/smn33/scripts/NBS_benchmarking/NBS_benchmarking/support_scripts/'; 
-
- end
-case 'steph'
     
-    switch data_origin
-    case 'mrrc'
-        data_dir='/Users/steph/Documents/data/mnt/hcp_1200/matrices/'; % mounted smn33 dir
-        output_dir='/Users/steph/Documents/data/mnt/NBS_benchmarking_results/';
-        %output_dir='/Users/steph/Steph-Lab/NBS_benchmarking/results_benchmarking/'; % local results
-    case 'farnam'
-        data_dir='/Users/steph/Documents/data/mnt/project/HCP_S1200/'; % mounted
-        output_dir='/Users/steph/Documents/data/mnt/project/benchmarking_results/';
-    end
-    
-    nbs_dir='/Users/steph/Steph-Lab/Misc/Software/scripts/Matlab/fmri/NBS1.2';
-    other_scripts_dir='/Volumes/GoogleDrive/My Drive/Steph-Lab/Misc/Software/scripts/Matlab/myscripts/NBS_benchmarking/NBS_benchmarking/support_scripts/';
-
-case 'ubuntu'
-    
-    data_dir='/home/ubuntu/data/matrices/';
-    output_dir='/home/ubuntu/data/NBS_benchmarking_results/';
-    nbs_dir='/home/ubuntu/scripts/NBS1.2/';
-    other_scripts_dir='/home/ubuntu/scripts/NBS_benchmarking/support_scripts/';
-
-otherwise
-    
-    error('Specified to use system-dependent paths but paths undefined.');
-    
+    case 'smn33'
+        
+        switch data_origin
+                
+            case 'farnam'
+                data_dir='/home/smn33/project/HCP_S1200/';
+                output_dir='/home/smn33/project/benchmarking_results/';
+                nbs_dir='/home/smn33/scripts/NBS1.2';
+                other_scripts_dir='/home/smn33/scripts/NBS_benchmarking/NBS_benchmarking/support_scripts/';
+                
+            case 'mrrc' % old
+                data_dir='/data15/mri_group/smn33_data/hcp_1200/matrices/'; % symlink to '/mnt/dustin/data/S1200/matrices/S1200/matrices/'
+                output_dir='/data15/mri_group/smn33_data/NBS_benchmarking_results/';
+                nbs_dir='/mridata2/home2/smn33/scripts/NBS1.2';
+                other_scripts_dir='/mridata2/home2/smn33/scripts/matlab/myscripts/general_mri_new/general_mri';
+                
+        end
+    case 'steph'
+        
+        switch data_origin
+            case 'farnam'
+                data_dir='/Users/steph/Documents/data/mnt/project/HCP_S1200/'; % mounted
+                output_dir='/Users/steph/Documents/data/mnt/project/benchmarking_results/';
+            case 'mrrc' % old
+                data_dir='/Users/steph/Documents/data/mnt/hcp_1200/matrices/'; % mounted smn33 dir
+                output_dir='/Users/steph/Documents/data/mnt/NBS_benchmarking_results/';
+                %output_dir='/Users/steph/Steph-Lab/NBS_benchmarking/results_benchmarking/'; % local results
+        end
+        
+        nbs_dir='/Users/steph/Steph-Lab/Misc/Software/scripts/Matlab/fmri/NBS1.2';
+        other_scripts_dir='/Volumes/GoogleDrive/My Drive/Steph-Lab/Misc/Software/scripts/Matlab/myscripts/NBS_benchmarking/NBS_benchmarking/support_scripts/';
+        
+    case 'ubuntu' % aws
+        
+        data_dir='/home/ubuntu/data/matrices/';
+        output_dir='/home/ubuntu/data/NBS_benchmarking_results/';
+        nbs_dir='/home/ubuntu/scripts/NBS1.2/';
+        other_scripts_dir='/home/ubuntu/scripts/NBS_benchmarking/support_scripts/';
+        
+    otherwise
+        
+        error('Specified to use system-dependent paths but paths undefined.');
+        
 end
 
 
