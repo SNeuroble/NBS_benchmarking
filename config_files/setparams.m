63001d44e9e9030cbddb25d31bf5bc1f929a05da
sneuroble <stephanie.noble@yale.edu>
2022-01-15 15:58:29 -0500

    More description of example paths

--- config_files/setparams.m
+++ config_files/setparams.m
@@ -8,20 +8,22 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % NBS toolbox
-nbs_dir='/Volumes/GoogleDrive/My Drive/Lab/Misc/Software/scripts/Matlab/fmri/NBS1.2/';
+nbs_dir='/home/smn33/scripts/NBS1.2';
 
-% Data
-data_file='/Volumes/GoogleDrive/My Drive/Lab/Misc/Software/scripts/Matlab/fmri/NBS1.2/SchizophreniaExample/matrices.mat';
+% Data: n_nodes x n_nodes x n_subs
+% for testing, get from NBS toolbox "SchizophreniaExample" dir
+data_file='/home/smn33/scripts/NBS1.2/SchizophreniaExample/matrices.mat';
 
-% Model
-% file contains 2D matrix
-design_matrix_file='/Volumes/GoogleDrive/My Drive/Lab/Misc/Software/scripts/Matlab/fmri/NBS1.2/SchizophreniaExample/designMatrix.mat';
+% Model: 2D matrix
+% for testing, get from NBS toolbox "SchizophreniaExample" dir
+design_matrix_file='/home/smn33/scripts/NBS1.2/SchizophreniaExample/designMatrix.mat';
 contrast=[-1,1];
 exchange=[];
 
 % Edge groups -- required for cNBS
 % file contains n_nodes x n_nodes edge matrix mask with nonzeros as follows: 1=subnetwork 1, 2=subnetwork 2, etc.
-edge_groups_file='/Volumes/GoogleDrive/My Drive/Lab/Misc/Software/scripts/Matlab/myscripts/NBS_benchmarking/NBS_addon/SchizophreniaExample/Example_74node_map.mat'; 
+% for testing, get a map from this NBS_benchmarking toolbox "NBS_addon" dir
+edge_groups_file='/home/smn33/scripts/NBS_benchmarking/NBS_addon/SchizophreniaExample/Example_74node_map.mat'; 
 
 % NBS parameters
 nbs_method='Run NBS';
@@ -31,7 +33,7 @@ n_perms=1000;               % recommend n_perms=5000 to appreciably reduce uncer
 tthresh_first_level=3.1;    % t=3.1 corresponds with p=0.005-0.001 (DOF=10-1000)
                             % Only used if cluster_stat_type='Size'
 pthresh_second_level=0.05;  % FWER or FDR rate
-cluster_stat_type='Constrained';    % 'Parametric_Bonferroni' | 'Parametric_FDR' | 'Size' | 'TFCE' | 'Constrained' | 'Constrained_FDR' | 'Omnibus' | 'SEA' (under construction)
+cluster_stat_type='Size';    % 'Parametric_Bonferroni' | 'Parametric_FDR' | 'Size' | 'TFCE' | 'Constrained' | 'Constrained_FDR' | 'Omnibus' | 'SEA' (under construction)
 cluster_size_type='Extent'; % 'Intensity' | 'Extent'
                             % Only used if cluster_stat_type='Size'
 omnibus_type='Threshold_Positive';  % 'Threshold_Positive' | 'Threshold_Both_Dir' | 'Average_Positive' | 'Average_Both_Dir' | 'Multidimensional_cNBS' | 'Multidimensional_all_edges' 
